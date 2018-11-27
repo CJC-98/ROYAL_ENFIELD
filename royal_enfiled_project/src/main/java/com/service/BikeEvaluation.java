a877232590ee22ac716e3fd3663765faef072ea2
prakohad@gmail.com <prakohad@gmail.com>
2018-11-27 15:04:25 +0530

    * cloud repository configuration is added * valuation service in progress

---
+++ royal_enfiled_project/src/main/java/com/service/BikeEvaluation.java
@@ -0,0 +1,14 @@
+package com.service;
+
+public class BikeEvaluation {
+	
+	public long[] getBikeEvaluation(String model, String state, String yearOfManifacture, long  kilometerRun) {
+		
+		long price[];
+		
+		
+		
+		return null;
+	}
+	
+}
