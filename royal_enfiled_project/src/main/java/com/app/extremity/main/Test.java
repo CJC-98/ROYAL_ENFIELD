0839eb420900f5f6b3b53744495e6e6ce8ce0e5c
prakohad@gmail.com <prakohad@gmail.com>
2018-11-30 11:54:49 +0530

    added

--- royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
@@ -21,7 +21,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages="com")
 @EntityScan("com.app.extremity.model")
 @EnableJpaRepositories("com.app.extremity.idao") 
-
+   
 public class Test {    
 	public static void main(String[] args) {
 		SpringApplication.run(Test.class);
