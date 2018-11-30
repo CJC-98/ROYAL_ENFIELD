26b3d6e2a7aa9cfd331153ecf09497b4b3d722a3
prakohad@gmail.com <prakohad@gmail.com>
2018-11-30 12:24:40 +0530

    added in master

--- royal_enfiled_project/src/main/java/com/app/extremity/controller/HomeController.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //author: pranay kohad
-
+  
 @Controller    
 public class HomeController {
 	    
