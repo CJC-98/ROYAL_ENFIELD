f09824950fe25e01c501e60b4c33593fc6704625
prakohad@gmail.com <prakohad@gmail.com>
2018-11-30 20:18:04 +0530

    preanaykohad merged into serviceteam

--- royal_enfiled_project/src/main/java/com/app/extremity/controller/ServiceMangerController.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/controller/ServiceMangerController.java
@@ -3,7 +3,7 @@ package com.app.extremity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+  
 /* 
  * This controller helps to navigate in service manager index.jsp
  * and handle all request-response made by service manager
