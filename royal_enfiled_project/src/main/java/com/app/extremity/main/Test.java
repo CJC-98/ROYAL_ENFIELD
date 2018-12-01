7f10bfc382e1e0dabfdda30051cef4d44ae116a9
samirdeshmukh700 <samir_shine@rediffmail.com>
2018-12-01 17:36:33 +0530

    account folder added

--- royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
   
                  
-//@SpringBootApplication(scanBasePackages="com")
+//@SpringBootApplication(scanBasePackages="com") 
 //         
 //@EntityScan("com.model")
 //@EnableJpaRepositories("com.Idao") 
