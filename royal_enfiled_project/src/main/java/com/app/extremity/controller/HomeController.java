55c3b4734a43b6cff16f8c245e7c62d42455a3e0
samirdeshmukh700 <samir_shine@rediffmail.com>
2018-12-03 15:02:40 +0530

    commit of latest

--- royal_enfiled_project/src/main/java/com/app/extremity/controller/HomeController.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//author: pranay kohad
+//author: pranay kohad 
     
 @Controller    
 public class HomeController {
