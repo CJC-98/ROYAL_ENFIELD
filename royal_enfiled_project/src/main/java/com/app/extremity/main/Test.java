4a4debbcdd54d858517a77412bcaee8cfaa3e18a
samirdeshmukh700 <samir_shine@rediffmail.com>
2018-12-03 15:18:19 +0530

    commit for removing comment in home controller

--- royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
@@ -1,21 +1,16 @@
 package com.app.extremity.main;
 
-//import org.apache.struts.tiles.actions.TilesAction;
+
 import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.SpringBootConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.boot.test.context.TestConfiguration;
+
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
   
                  
-//@SpringBootApplication(scanBasePackages="com") 
-//         
-//@EntityScan("com.model")
-//@EnableJpaRepositories("com.Idao") 
-//@ComponentScan(basePackages= {"com.app.extremity"})  
+
    
 
 @SpringBootApplication(scanBasePackages="com")
