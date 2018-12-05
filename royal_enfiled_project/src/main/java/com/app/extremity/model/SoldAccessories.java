5f77eedc870ab9a69b796a9dec26d9e8239eec5d
shashank8983 <mandlik.shashank@gmail.com>
2018-12-05 16:01:31 +0530

    Final Pojo reflected

---
+++ royal_enfiled_project/src/main/java/com/app/extremity/model/SoldAccessories.java
@@ -0,0 +1,67 @@
+package com.app.extremity.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+//to sold accessories
+//admin and sale only..
+@Entity
+public class SoldAccessories {
+	@Id
+	//@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String partId;
+	private String partName;
+	private String partPrice;
+	private long partQuantity;
+	@OneToOne(cascade = CascadeType.ALL)
+	private BikeModel bikeModel;
+	//@OneToMany(cascade = CascadeType.ALL)
+	//private User user;
+
+	public String getPartId() {
+		return partId;
+	}
+
+	public void setPartId(String partId) {
+		this.partId = partId;
+	}
+
+	public String getPartName() {
+		return partName;
+	}
+
+	public void setPartName(String partName) {
+		this.partName = partName;
+	}
+
+	public String getPartPrice() {
+		return partPrice;
+	}
+
+	public void setPartPrice(String partPrice) {
+		this.partPrice = partPrice;
+	}
+
+	public long getPartQuantity() {
+		return partQuantity;
+	}
+
+	public void setPartQuantity(long partQuantity) {
+		this.partQuantity = partQuantity;
+	}
+
+	public BikeModel getBikeModel() {
+		return bikeModel;
+	}
+
+	public void setBikeModel(BikeModel bikeModel) {
+		this.bikeModel = bikeModel;
+	}
+
+}
