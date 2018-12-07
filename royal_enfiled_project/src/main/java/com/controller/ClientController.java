bc02cbbd99f35fdbf871845683a12245be8d1a33
alivinghero <alivinghero98@gmail.com>
2018-12-07 10:06:32 +0530

    changes in signup page and index page

--- royal_enfiled_project/src/main/java/com/controller/ClientController.java
+++ royal_enfiled_project/src/main/java/com/controller/ClientController.java
@@ -1,8 +1,18 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ClientController {
+	
+	@RequestMapping(value="/saleBike")
+	public String saleUsedBike()
+	{  
+		System.out.println("In sale Bike Controller");
+		
+		return "IndivisualUser/saleUsedBike";
+	}
+
 
 }
