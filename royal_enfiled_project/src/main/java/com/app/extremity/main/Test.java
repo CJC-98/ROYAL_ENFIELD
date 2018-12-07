9a0d9d9aa1a45e703a8c0b7c9e3c0ab565bd1620
vspande13 <vspande4@gmail.com>
2018-12-07 17:45:58 +0530

    megrge master

--- royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+ 
 
 @SpringBootApplication(scanBasePackages="com")
 @EntityScan("com.app.extremity.model")
