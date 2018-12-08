d5575d04b96675dfab7575ad48b51c52768b11b2
prakohad@gmail.com <prakohad@gmail.com>
2018-12-08 19:36:53 +0530

    logs

--- royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
@@ -1,7 +1,7 @@
 package com.app.extremity.main;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Logger;  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
