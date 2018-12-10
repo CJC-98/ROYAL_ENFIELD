1b68bf2c6732c6006a99b09c71eb25050dc185ec
sandip <rsandip2591@gmail.com>
2018-12-10 18:17:34 +0530

    sandip's branch

--- royal_enfiled_project/src/main/java/com/app/extremity/controller/SalesManagerController.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/controller/SalesManagerController.java
@@ -3,7 +3,7 @@ package com.app.extremity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//******    
 @Controller
 public class SalesManagerController {
 
