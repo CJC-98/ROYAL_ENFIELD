e96d03fb59c20717a0a50f15c9e1d3d730c95cd1
MANOJgj <7276761439.mj@gmail.com>
2018-12-10 16:48:01 +0530

    all changes in methods and mapping...

--- royal_enfiled_project/src/main/java/com/app/extremity/controller/SalesManagerController.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/controller/SalesManagerController.java
@@ -15,7 +15,7 @@ public class SalesManagerController {
 		return "SalesManager/salesManagerIndex";
 	}
 
-	@RequestMapping(value = "/AddNewBike")
+	@RequestMapping(value = "/AddnewBike")
 	public String AddNewBikeForm(Model model) {
 		System.out.println("in new bike form");
 		model.addAttribute("link", "AddNewBike.jsp");
@@ -24,7 +24,7 @@ public class SalesManagerController {
 
 	}
 
-	@RequestMapping(value = "/AddForm")
+	@RequestMapping(value = "/AddAccessories")
 	public String AddNewAccessoriesForm(Model model) {
 
 		System.out.println("in new accessories form");
@@ -33,30 +33,138 @@ public class SalesManagerController {
 
 	}
 
-	@RequestMapping(value = "/OrderToCompanyForNewBike")
-	public String OrderToCompany(Model model) {
+	@RequestMapping(value = "/Offers")
+	public String Order(Model model) {
 
-		System.out.println("in new Bike form to company ");
-		model.addAttribute("link", "OrderToCompanyForNewBike.jsp");
+		System.out.println("in order");
+		model.addAttribute("link", "Offers.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/DeadAccessories")
+	public String DeadAccessories(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "DeadAccessories.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/DeadBike")
+	public String DeadBike(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "DeadBike.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewOldBike")
+	public String ViewOldBike(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewOldBike.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewNewBike")
+	public String ViewNewBike(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewOldBike.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewAccessories")
+	public String ViewAccessories(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewAccessories.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewInvoice")
+	public String ViewInvoice(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewInvoice.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewOffer")
+	public String ViewOffer(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewOffer.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewSoldAccessories")
+	public String ViewSoldAccessories(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewSoldAccessories.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewSoldNewBike")
+	public String ViewSoldNewBike(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewSoldNewBike.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewSoldOldBike")
+	public String ViewSoldOldBike(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewSoldOldBike.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewDeadStockAccessories")
+	public String ViewDeadStockAccessories(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewDeadStockAccessories.jsp");
+		return "SalesManager/salesManagerIndex";
+
+	}
+
+	@RequestMapping(value = "/ViewDeadStockBike")
+	public String ViewDeadStockBike(Model model) {
+
+		System.out.println("in order");
+		model.addAttribute("link", "ViewDeadStockBike.jsp");
 		return "SalesManager/salesManagerIndex";
 
 	}
 
 	@RequestMapping(value = "/OrderToCompanyForNewAccessories")
-	public String OrderToCompanyForAccessories(Model model) {
+	public String OrderToCompanyForNewAccessories(Model model) {
 
-		System.out.println("in new Accessories form to company ");
+		System.out.println("in order");
 		model.addAttribute("link", "OrderToCompanyForNewAccessories.jsp");
 		return "SalesManager/salesManagerIndex";
 
 	}
 
-	@RequestMapping(value = "/order")
-	public String Order(Model model) {
+	@RequestMapping(value = "/OrderToCompanyForNewBike")
+	public String OrderToCompanyForNewBike(Model model) {
 
 		System.out.println("in order");
+		model.addAttribute("link", "OrderToCompanyForNewBike.jsp");
 		return "SalesManager/salesManagerIndex";
 
 	}
-
 }
--- royal_enfiled_project/src/main/webapp/WEB-INF/JSP_Pages/SalesManager/AddAccessoriesInDeadStock.jsp
+++ royal_enfiled_project/src/main/webapp/WEB-INF/JSP_Pages/SalesManager/AddAccessoriesInDeadStock.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-
 <!DOCTYPE html>
 <html lang="en" class="bg-dark">
 <!-- Mirrored from flatfull.com/themes/note/signup.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 19 Oct 2018 11:17:05 GMT -->
@@ -13,34 +12,49 @@
     <link rel="stylesheet" href="css/font.css" type="text/css" />
     <link rel="stylesheet" href="css/app.v1.css" type="text/css" />
     <!--[if lt IE 9]> <script src="js/ie/html5shiv.js"></script> <script src="js/ie/respond.min.js"></script> <script src="js/ie/excanvas.js"></script> <![endif]-->
+    <script type="text/javascript">
+
+
+    </script>
 </head>
 
 <body class="">
     <section id="content" class="m-t-lg wrapper-md animated fadeInDown">
-        <div class="container aside-xxl"> <a class="navbar-brand block" href="index.html">Add Accessories Form</a>
+        <div class="container aside-xxl"> <a class="navbar-brand block" href="index.html">Add Accessories DeadStock </a>
             <section class="panel panel-default m-t-lg bg-white">
                 <header class="panel-heading text-center"> <strong>Add Accessories</strong> </header>
                 <form action="http://flatfull.com/themes/note/index.html" class="panel-body wrapper-lg">
-                    <div class="form-group"> <label class="control-label">Bike Model</label> <input type="text" name="bikeModel" placeholder="Enter Bike Model"
-                            class="form-control input-lg"> </div>
-                    <div class="form-group"> <label class="control-label">Accessories type</label> <input type="text" name="accessoriesType" placeholder="Enter Accessories Type"
-                            class="form-control input-lg"> </div>
-                   
-                    <p class="text-muted text-center"></p> <button name="addAccessories" id="addAccessories"
-                        class="btn btn-primary">Add Accessories</button>
+                    <div class="form-group"> <label class="control-label"> Part Name</label> <input type="text" name="partName"
+                            placeholder="Enter part name" class="form-control input-lg"> </div>
+                    <div class="form-group"> <label class="control-label">Part Price </label> <input type="text" name="partPrice"
+                            placeholder="Enter part price" class="form-control input-lg"> </div>
+
+
+                    <div class="form-group"> <label class="col-sm-2 control-label">Quantity</label>
+                        <div class="col-sm-10">
+
+                            <input type='text' name='qty' id='qty'/>
+                            <input type='button' name='add' onclick='javascript: document.getElementById("qty").value++;'
+                                value='+' />
+                            <input type='button' name='subtract' onclick='javascript: document.getElementById("qty").value--;'
+                                value='-' />
+
+                        </div>
+                    </div>
+                    <p class="text-muted text-center"></p> <button name="addBike" id="addBike" class="btn-success">Add Bike</button>
                 </form>
             </section>
         </div>
     </section> <!-- footer -->
-   <!--  <footer id="footer">
+    <footer id="footer">
         <div class="text-center padder clearfix">
             <p> <small>Web app framework base on Bootstrap<br>&copy; 2013</small> </p>
         </div>
-    </footer> --> <!-- / footer -->
+    </footer> <!-- / footer -->
     <!-- Bootstrap -->
     <!-- App -->
-   <script src="js/app.v1.js"></script>
-    <script src="js/app.plugin.js"></script>
+    <!-- <script src="js/app.v1.js"></script>-->
+    <!--   <script src="js/app.plugin.js"></script>-->
 </body>
 <!-- Mirrored from flatfull.com/themes/note/signup.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 19 Oct 2018 11:17:05 GMT -->
 
