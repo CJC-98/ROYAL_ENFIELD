1ca84467be5f5cefede2c701d148298b21d745d1
prakohad@gmail.com <prakohad@gmail.com>
2018-12-11 11:44:07 +0530

    delear and indivisual user pojo added

---
+++ royal_enfiled_project/src/main/java/com/app/extremity/model/Role.java
@@ -0,0 +1,42 @@
+package com.app.extremity.model;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+/**
+ * @author Individual User & Dealer;
+ * This pojo class is used for Role purpose;
+ * Role reference is given in Login pojo
+ */
+
+@Entity
+public class Role 
+{	
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int roleId;
+	
+	private String roleName;
+	
+	
+	
+	
+	public int getRoleId() {
+	return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}	
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+	
+}
