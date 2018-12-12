86dc08b0f97ef92ec2d1897462ec816816df758a
Your Name <you@example.com>
2018-12-12 14:34:50 +0530

    no

--- royal_enfiled_project/src/main/java/com/app/extremity/controller/DealerController.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/controller/DealerController.java
@@ -6,7 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DealerController {
-  //Buy new Bike Page
+	@RequestMapping(value="/DealerDashboardPage")
+	public String signIn(Model model)    
+	{ 
+		
+		System.out.println("dashboard hits...........");
+		model.addAttribute("link","dealerDashboard.jsp");
+	
+		System.out.println("In SignIn controller");
+		return "Dealer/dealerIndex";//by default go to client index.jsp  
+
+	} 
+	
+	
+	//Buy new Bike Page
 	@RequestMapping(value="/NewBike1")
 	public String NewBike(Model model){
 		
@@ -37,6 +50,14 @@ public class DealerController {
 		
 		System.out.println("newbike hits...........");   
 		model.addAttribute("link","Accessories.jsp");
+      System.out.println("final");
+		return "Dealer/dealerIndex";
+	}
+	@RequestMapping(value="/cart")
+	public String cart(Model model){
+		
+		System.out.println("newbike hits...........");   
+		model.addAttribute("link","cart.jsp");
       System.out.println("final");
 		return "Dealer/dealerIndex";
 	}
