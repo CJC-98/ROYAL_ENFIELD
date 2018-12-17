a420fdc0d77f503adcda8eda6a948479b491b411
MANOJgj <7276761439.mj@gmail.com>
2018-12-17 14:21:07 +0530

    hggb

---
+++ royal_enfiled_project/src/main/java/com/app/extremity/model/AddAccessories.java
@@ -0,0 +1,4 @@
+package com.app.extremity.model;
+public class AddAccessories {
+
+}
---
+++ royal_enfiled_project/src/main/java/com/app/extremity/model/OrderToCompanyForAccessories.java
@@ -0,0 +1,64 @@
+package com.app.extremity.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class OrderToCompanyForAccessories
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+private int companyOrderId;
+private String orderDate;
+@OneToOne(cascade=CascadeType.ALL)
+@JoinColumn(name="bikeModelId")
+private BikeModel bikemodel;
+private String quantity;
+//private String totalPrice;
+@OneToOne(cascade=CascadeType.ALL)
+@JoinColumn(name="accId")
+private AddAccessories addAccessories;
+
+public AddAccessories getAddAccessories() {
+	return addAccessories;
+}
+public void setAddAccessories(AddAccessories addAccessories) {
+	this.addAccessories = addAccessories;
+}
+public int getCompanyOrderId() {
+	return companyOrderId;
+}
+public void setCompanyOrderId(int companyOrderId) {
+	this.companyOrderId = companyOrderId;
+}
+public String getOrderDate() {
+	return orderDate;
+}
+public void setOrderDate(String orderDate) {
+	this.orderDate = orderDate;
+}
+public BikeModel getBikemodel() {
+	return bikemodel;
+}
+public void setBikemodel(BikeModel bikemodel) {
+	this.bikemodel = bikemodel;
+}
+public String getQuantity() {
+	return quantity;
+}
+public void setQuantity(String quantity) {
+	this.quantity = quantity;
+}
+/*public String getTotalPrice() {
+	return totalPrice;
+}
+public void setTotalPrice(String totalPrice) {
+	this.totalPrice = totalPrice;
+}*/
+}
