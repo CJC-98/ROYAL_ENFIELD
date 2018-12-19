4d9fb57de9ba4906d291bf65a92fe69d1debaf3e
panduranggiram <girampg0105@gmail.com>
2018-12-19 15:18:19 +0530

    change in test

--- royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
+++ royal_enfiled_project/src/main/java/com/app/extremity/main/Test.java
@@ -20,7 +20,7 @@ public class Test {
 	public static void main(String[] args) {
 		SpringApplication.run(Test.class);
 		logger.warn("application started............");
-		System.out.println("application started...........!!");
+		System.out.println("application started...........!!!!!");
 
 	}
 }
